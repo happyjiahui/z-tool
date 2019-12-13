c7c86eefc2b692729638a9833c139f1dd3838144
zhaojinbing <beyondjinbing@hotmail.com>
2019-12-13 10:29:47 +0800

    feat: 改造rocksdb工具类为普通类

--- z-core/src/main/java/com/github/happyjiahui/z/rocksDb/RocksDbHelp.java
+++ z-core/src/main/java/com/github/happyjiahui/z/rocksDb/RocksDbHelp.java
@@ -6,61 +6,35 @@
  * PURPOSE. See the Mulan PSL v1 for more details.
  */
 
-package com.github.happyjiahui.z.util;
+package com.github.happyjiahui.z.rocksDb;
 
 import java.io.Serializable;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.rocksdb.InfoLogLevel;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.happyjiahui.z.exception.UtilException;
 import com.github.happyjiahui.z.util.FstUtils;
 
-/**
- * @author beyondjinbing
- * @version 0.1.7
- */
-public class RocksDbUtils {
+public class RocksDbHelp {
 
-    private RocksDbUtils() {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RocksDbHelp.class);
+    private static final ConcurrentHashMap<String, RocksDB> ROCKS_DB_MAP = new ConcurrentHashMap<>();
 
+    public RocksDbHelp(String dbName, String dbPath) {
+        init(dbName, dbPath, null);
     }
 
-    /**
-     * 初始化rocksDb
-     * 
-     * @param dbPath
-     *            db路径
-     * @param options
-     *            rocksDb配置 {@link Options}
-     * @return {@link RocksDB}实例
-     */
-    public static RocksDB init(String dbPath, Options options) {
-        RocksDB.loadLibrary();
-        if (options == null) {
-            options = getDefaultOptions();
-        }
-        try {
-            return RocksDB.open(options, dbPath);
-        } catch (RocksDBException e) {
-            throw new UtilException("rocksDb 初始化失败");
-        }
+    public RocksDbHelp(String dbName, String dbPath, Options options) {
+        init(dbName, dbPath, options);
     }
 
-    /**
-     * 初始化rocksDb
-     * 
-     * @param dbPath
-     *            db路径
-     * @return {@link RocksDB}实例
-     */
-    public static RocksDB init(String dbPath) {
-        return init(dbPath, null);
-    }
-
-    private static Options getDefaultOptions() {
+    private Options getDefaultOptions() {
         Options options = new Options();
         options.setNewTableReaderForCompactionInputs(true);
         options.setCreateIfMissing(true);
@@ -83,20 +57,54 @@ public class RocksDbUtils {
     }
 
     /**
-     * 关闭数据库
-     * 
-     * @param rocksDB
-     *            {@link RocksDB}实例
+     * 初始化rocksDb
+     *
+     * @param dbName
+     *            数据库名称
+     * @param dbPath
+     *            数据库路径
+     * @param options
+     *            rocksDb配置 {@link Options}
      */
-    public static void close(RocksDB rocksDB) {
-        rocksDB.close();
+    private void init(String dbName, String dbPath, Options options) {
+        boolean isExits = ROCKS_DB_MAP.containsKey(dbName);
+        if (isExits) {
+            LOGGER.info("数据库{}已经存在，直接使用。", dbName);
+            return;
+        }
+        RocksDB.loadLibrary();
+        if (options == null) {
+            options = getDefaultOptions();
+        }
+        try {
+            RocksDB rocksDB = RocksDB.open(options, dbPath);
+            ROCKS_DB_MAP.putIfAbsent(dbName, rocksDB);
+            LOGGER.info("数据库{}初始化完成。", dbName);
+        } catch (RocksDBException e) {
+            throw new UtilException("rocksDb 初始化失败");
+        }
+    }
+
+    /**
+     * 获取数据库实例
+     * 
+     * @param dbName
+     *            数据库名称
+     * @return 数据库实例 {@link RocksDB}
+     */
+    private RocksDB getRocksDb(String dbName) {
+        RocksDB rocksDB = ROCKS_DB_MAP.get(dbName);
+        if (rocksDB == null) {
+            throw new UtilException("没有找到对应的数据库");
+        }
+        return rocksDB;
     }
 
     /**
      * 从数据库中放入值
-     * 
-     * @param rocksDB
-     *            {@link RocksDB}实例
+     *
+     * @param dbName
+     *            数据库名称
      * @param key
      *            key值
      * @param value
@@ -104,7 +112,8 @@ public class RocksDbUtils {
      * @param <T>
      *            value类型
      */
-    public static <T extends Serializable> void put(RocksDB rocksDB, String key, T value) {
+    public <T extends Serializable> void put(String dbName, String key, T value) {
+        RocksDB rocksDB = getRocksDb(dbName);
         byte[] keyBytes = FstUtils.serializer(key);
         byte[] valueBytes = FstUtils.serializer(value);
 
@@ -117,16 +126,18 @@ public class RocksDbUtils {
 
     /**
      * 从数据库中取值
-     * 
-     * @param rocksDB
-     *            {@link RocksDB}实例
+     *
+     * @param dbName
+     *            数据库名称
      * @param key
      *            key值
      * @param <T>
      *            value类型
      * @return value
      */
-    public static <T extends Serializable> T get(RocksDB rocksDB, String key) {
+    public <T extends Serializable> T get(String dbName, String key) {
+        RocksDB rocksDB = getRocksDb(dbName);
+
         byte[] keyBytes = FstUtils.serializer(key);
         try {
             byte[] bytes = rocksDB.get(keyBytes);
@@ -138,19 +149,33 @@ public class RocksDbUtils {
 
     /**
      * 从数据库中删除值
-     * 
-     * @param rocksDB
-     *            {@link RocksDB}实例
+     *
+     * @param dbName
+     *            数据库名称
      * @param key
      *            key值
      */
-    public static void delete(RocksDB rocksDB, String key) {
+    public void delete(String dbName, String key) {
+        RocksDB rocksDB = getRocksDb(dbName);
+
         byte[] keyBytes = FstUtils.serializer(key);
         try {
             rocksDB.delete(keyBytes);
         } catch (RocksDBException e) {
             throw new UtilException("从rocksDb中删除值失败", e);
         }
+    }
+
+    /**
+     * 关闭数据库
+     *
+     * @param dbName
+     *            数据库名称
+     */
+    public void close(String dbName) {
+        RocksDB rocksDB = getRocksDb(dbName);
+        rocksDB.close();
+        ROCKS_DB_MAP.remove(dbName);
     }
 
 }
--- z-core/src/test/java/com/github/happyjiahui/z/rocksdb/RocksDbUtilsTest.java
+++ z-core/src/test/java/com/github/happyjiahui/z/rocksdb/RocksDbUtilsTest.java
@@ -1,20 +1,55 @@
 package com.github.happyjiahui.z.rocksdb;
 
-import com.github.happyjiahui.z.util.RocksDbUtils;
+import java.math.BigDecimal;
+
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
-import org.rocksdb.RocksDB;
+import org.junit.rules.ExpectedException;
+
+import com.github.happyjiahui.z.exception.UtilException;
+import com.github.happyjiahui.z.rocksDb.RocksDbHelp;
 
 public class RocksDbUtilsTest {
+
+    private static String dbName = "test";
+    private static RocksDbHelp rocksDbHelp;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        rocksDbHelp = new RocksDbHelp(dbName, "rocksDbTest");
+    }
 
     @Test
     public void test() {
         String value = "小明";
-        RocksDB rocksDB = RocksDbUtils.init("rocksDbTest");
-        RocksDbUtils.put(rocksDB, "a", value);
-        String a = RocksDbUtils.get(rocksDB, "a");
-        RocksDbUtils.close(rocksDB);
+        rocksDbHelp.put(dbName, "a", value);
+        String a = rocksDbHelp.get(dbName, "a");
         Assert.assertEquals(a, value);
+    }
+
+    @Test
+    public void test2() {
+        rocksDbHelp = new RocksDbHelp(dbName, "rocksDbTest");
+        BigDecimal bigDecimal = BigDecimal.valueOf(20.12);
+        rocksDbHelp.put(dbName, "b", bigDecimal);
+        BigDecimal value = rocksDbHelp.get(dbName, "b");
+        Assert.assertEquals(value, bigDecimal);
+        rocksDbHelp.delete(dbName, "b");
+        BigDecimal value2 = rocksDbHelp.get(dbName, "b");
+        Assert.assertNull(value2);
+    }
+
+    @Test
+    public void test3() {
+        rocksDbHelp.close(dbName);
+        thrown.expect(UtilException.class);
+        thrown.expectMessage("没有找到对应的数据库");
+        rocksDbHelp.put(dbName, "c", "hello world");
     }
 
 }
